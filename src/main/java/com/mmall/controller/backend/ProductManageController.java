a6abea70ff7ad8f33353874db034f7855ad0120f
LiFH <fh.li@foxmail.com>
2018-05-04 23:35:42 +0800

    admin

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -122,10 +122,10 @@ public class ProductManageController {
     @ResponseBody
     public ServerResponse upload(HttpSession session,@RequestParam(value = "upload_file",required = false) MultipartFile file,HttpServletRequest request){
         User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
-        }
-        if(iUserService.checkAdminRole(user).isSuccess()){
+//        if(user == null){
+//            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+//        }
+//        if(iUserService.checkAdminRole(user).isSuccess()){
             String path = request.getSession().getServletContext().getRealPath("upload");
             String targetFileName = iFileService.upload(file,path);
             String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
@@ -134,9 +134,9 @@ public class ProductManageController {
             fileMap.put("uri",targetFileName);
             fileMap.put("url",url);
             return ServerResponse.createBySuccess(fileMap);
-        }else{
-            return ServerResponse.createByErrorMessage("无权限操作");
-        }
+//        }else{
+//            return ServerResponse.createByErrorMessage("无权限操作");
+//        }
     }
 
 
